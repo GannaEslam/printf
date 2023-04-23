a9b294ae3822e1c485f3b937f1ec98220c14a3f7
GannaEslam <125409383+GannaEslam@users.noreply.github.com>
2023-04-23 20:30:13 +0200

    _printf.c

---
+++ _printf.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _printf - Receives the main string and all the necessary parameters to
+ * print a formatted string
+ * @format: A string containing all the desired characters
+ * Return: A total count of the characters printed
+ */
+int _printf(const char *format, ...)
+{
+        int printed_chars;
+        va_list args;
+        conver_t ls[] = {
+                {"c", _putc},
+                {"s", print_string},
+                {NULL, NULL}
+        };
+
+        if (format == NULL)
+        {
+                return (-1);
+        }
+
+        va_start(args, format);
+        printed_chars = parser(format, ls, args);
+        va_end(args);
+
+        return (printed_chars);
+}
